fb5d3b018a073ee5a3a7ed8568b33461470029ca
coolshou <coolshou2060@gmail.com>
2024-05-09 11:47:32 +0800

    update build script

--- build-mingw64-x86.sh
+++ build-mingw64-x86.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 cmake -DCMAKE_TOOLCHAIN_FILE=./mingw-w64-x86.cmake -B build-mingw64-x86 -S .
+cd build-mingw64-x86
+make clean
+make
+cd ..
--- build-mingw64.sh
+++ build-mingw64.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 cmake -DCMAKE_TOOLCHAIN_FILE=./mingw-w64-x86_64.cmake -B build-mingw64 -S .
+cd build-mingw64
+make clean
+make
+cd ..
